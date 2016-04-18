a02b083dd4796dd64ff4229a907b1af461892b5f
Greg Lieske <grzesiu.lieske@gmail.com>
2016-04-18 15:25:13 +0200

    Lab3 v1

---
+++ Lab3/PhoneApp6/PhoneApp6/MainPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using PhoneApp6.Resources;
+using Windows.Devices.Geolocation;
+using System.IO.IsolatedStorage;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Device.Location;
+using Microsoft.Phone.Maps.Controls;
+using Microsoft.Phone.Maps.Toolkit;
+
+namespace PhoneApp6
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        GeoCoordinateWatcher watcher;
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            if (watcher == null)
+            {
+                watcher = new GeoCoordinateWatcher(GeoPositionAccuracy.High);
+                watcher.MovementThreshold = 1;
+                watcher.StatusChanged += new EventHandler<GeoPositionStatusChangedEventArgs>(watcher_StatusChanged);
+                watcher.PositionChanged += new EventHandler<GeoPositionChangedEventArgs<GeoCoordinate>>(watcher_PositionChanged);
+            }
+            watcher.Start();
+        }
+
+        void watcher_StatusChanged(object sender, GeoPositionStatusChangedEventArgs e)
+        {
+            switch (e.Status)
+            {
+                case GeoPositionStatus.Disabled:
+                    MessageBox.Show("Location Service is not enabled on the device");
+                    break;
+
+                case GeoPositionStatus.NoData:
+                    MessageBox.Show(" The Location Service is working, but it cannot get location data.");
+                    break;
+            }
+        }
+
+        void watcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+        {
+            if (e.Position.Location.IsUnknown)
+            {
+                MessageBox.Show("Please wait while your prosition is determined....");
+                return;
+            }
+            txt_lat.Text = watcher.Position.Location.ToString();
+
+            this.map.Center = new GeoCoordinate(e.Position.Location.Latitude, e.Position.Location.Longitude);
+            map.ZoomLevel = 7;
+            MapLayer layer1 = new MapLayer();
+
+            Pushpin pushpin1 = new Pushpin();
+
+            pushpin1.GeoCoordinate = e.Position.Location;
+            pushpin1.Content = "Tu jestem";
+            MapOverlay overlay1 = new MapOverlay();
+            overlay1.Content = pushpin1;
+            overlay1.GeoCoordinate = e.Position.Location;
+            layer1.Add(overlay1);
+
+            map.Layers.Add(layer1);
+
+
+        }
+    }
+}
