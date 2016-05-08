7d8e1efafaa13285df22cd7c6cda47d0f6ce459b
Greg Lieske <grzesiu.lieske@gmail.com>
2016-05-08 22:00:44 +0200

    Lab3 RTM

--- Lab3/PhoneApp6/PhoneApp6/MainPage.xaml.cs
+++ Lab3/PhoneApp6/PhoneApp6/MainPage.xaml.cs
@@ -1,20 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
-using PhoneApp6.Resources;
-using Windows.Devices.Geolocation;
-using System.IO.IsolatedStorage;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 using System.Device.Location;
 using Microsoft.Phone.Maps.Controls;
 using Microsoft.Phone.Maps.Toolkit;
@@ -24,16 +10,24 @@ namespace PhoneApp6
     public partial class MainPage : PhoneApplicationPage
     {
         GeoCoordinateWatcher watcher;
-        // Constructor
+        
         public MainPage()
         {
             InitializeComponent();
             if (watcher == null)
             {
                 watcher = new GeoCoordinateWatcher(GeoPositionAccuracy.High);
-                watcher.MovementThreshold = 1;
-                watcher.StatusChanged += new EventHandler<GeoPositionStatusChangedEventArgs>(watcher_StatusChanged);
+                watcher.MovementThreshold = 1; // ruch o ile metrów uznać jako zmianę
+                watcher.StatusChanged += new EventHandler<GeoPositionStatusChangedEventArgs>(watcher_StatusChanged); 
+                // przypisanie eventu dotyczącego zmiany statusu GPS (wyłączony itd.)
                 watcher.PositionChanged += new EventHandler<GeoPositionChangedEventArgs<GeoCoordinate>>(watcher_PositionChanged);
+                //przypisanie eventu dla zmiany pozycji
+            }
+            else
+            {
+                watcher.Stop();
+                watcher.Dispose();
+                GC.Collect();
             }
             watcher.Start();
         }
@@ -43,11 +37,11 @@ namespace PhoneApp6
             switch (e.Status)
             {
                 case GeoPositionStatus.Disabled:
-                    MessageBox.Show("Location Service is not enabled on the device");
+                    MessageBox.Show("Włącz GPS geniuszu");
                     break;
 
                 case GeoPositionStatus.NoData:
-                    MessageBox.Show(" The Location Service is working, but it cannot get location data.");
+                    MessageBox.Show(" GPS działa, ale coś sie zepsuło...");
                     break;
             }
         }
@@ -56,27 +50,34 @@ namespace PhoneApp6
         {
             if (e.Position.Location.IsUnknown)
             {
-                MessageBox.Show("Please wait while your prosition is determined....");
+                MessageBox.Show("Poczekaj, trwa wyszukiwanie pozycji...");
                 return;
             }
             txt_lat.Text = watcher.Position.Location.ToString();
 
             this.map.Center = new GeoCoordinate(e.Position.Location.Latitude, e.Position.Location.Longitude);
-            map.ZoomLevel = 7;
-            MapLayer layer1 = new MapLayer();
+            map.ZoomLevel = 15.7; // wartości double od 0.0 do 20.0 (minimalne do maksymalnego przybliżenia)
 
-            Pushpin pushpin1 = new Pushpin();
+            MapLayer layer1 = new MapLayer(); // utworzenie nakładki dla mapy
+
+            Pushpin pushpin1 = new Pushpin(); // utworzenie pinezki 
 
             pushpin1.GeoCoordinate = e.Position.Location;
-            pushpin1.Content = "Tu jestem";
-            MapOverlay overlay1 = new MapOverlay();
-            overlay1.Content = pushpin1;
+            pushpin1.Content = "Tu jestem"; // nazwa
+            MapOverlay overlay1 = new MapOverlay(); // utworzenie nakładki z elementami
+            overlay1.Content = pushpin1; // dodanie pinezki do nakładki z elementami
             overlay1.GeoCoordinate = e.Position.Location;
-            layer1.Add(overlay1);
+            layer1.Add(overlay1); //dodanie nakładki d;a mapy
 
-            map.Layers.Add(layer1);
+            map.Layers.Clear(); // usuniecie starych punktow
+            map.Layers.Add(layer1); // wstawienie nakładki mapy do mapy
+        }
 
-
+        private void map_Loaded(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Phone.Maps.MapsSettings.ApplicationContext.ApplicationId = "ApplicationID";
+            Microsoft.Phone.Maps.MapsSettings.ApplicationContext.AuthenticationToken = "AuthenticationToken";
+            //w to miejce można wstawić dane aplikacji do osługi map (płatne) tak aby nie wyświetlał się napis o niewłaściwym tokenie
         }
     }
 }
